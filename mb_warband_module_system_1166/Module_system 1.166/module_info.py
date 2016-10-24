f21b3c88782f35c434e7a10d5b82ad7006c29412
Sw4Tish <pierre.gambier@yahoo.fr>
2016-10-24 16:20:32 +0200

    Initial commit

---
+++ mb_warband_module_system_1166/Module_system 1.166/module_info.py
@@ -0,0 +1,6 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+#export_dir = "../WOTS/Modules/Native/"
+export_dir = "D:/SteamGames/steamapps/common/MountBlade Warband/Modules/Truand World 0.1/"
